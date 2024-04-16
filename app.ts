02615eed2abbb4b909f951f87a5618c0a06edbf6
Rameez Ali <ra2252252@gmail.com>
2024-04-17 01:22:13 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,21 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer:{
+    sentence : string
+} = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : "Enter you sentence to count the words"
+    }
+])
+
+let words = answer.sentence.trim().split(" ")
+
+
+console.log(words);
+
+console.log(`Your sentence word count is : ${words.length}`);
+
